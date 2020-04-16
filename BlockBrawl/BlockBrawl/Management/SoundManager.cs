eacbb87ed702e4bbc34ff57e27c6d7677c9098af
Richard <aj9321@mau.se>
2020-04-17 00:32:41 +0200

    Added soundmanager
    The soundmanager creates a filereader which looks for two folders: “noCopySounds” and “otherSounds” inside the folder from which the program executes. If these folders do not exist, the filereader will create them. The filereader will read the contents of both of these folders and place files that ends with “.wav” into two different string lists (the path is interesting here).
    
    The soundmanager will check both of these lists and create SoundEffects with the help of FileStream. The instances ends up in two different lists of type SoundEffectInstance. We can then play the sounds from these directorys inside our game. (Y)

---
+++ BlockBrawl/BlockBrawl/Management/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace BlockBrawl
+{
+    class SoundManager
+    {
+        private readonly List<SoundEffect> noCopySounds;
+        private readonly List<SoundEffect> otherSounds;
+
+        public SoundManager(ContentManager content)
+        {
+            noCopySounds = new List<SoundEffect>();
+            otherSounds = new List<SoundEffect>();
+
+            NoCopySounds = new List<SoundEffectInstance>();
+            OtherSounds = new List<SoundEffectInstance>();
+
+            List<string> tempList = new FileRead().NoCopySounds();
+            if (tempList.Count > 0)
+            {
+                for (int i = 0; i < tempList.Count; i++)
+                {
+                    var filestream = new FileStream(tempList[i], FileMode.Open);//Monogame community R0x
+                    using (filestream)
+                    {
+                        noCopySounds.Add(SoundEffect.FromStream(filestream));
+                    }
+                    NoCopySounds.Add(noCopySounds[i].CreateInstance());
+                }
+            }
+
+            tempList = new FileRead().OtherSounds();
+            if (tempList.Count > 0)
+            {
+                for (int i = 0; i < tempList.Count; i++)
+                {
+                    var filestream = new FileStream(tempList[i], FileMode.Open);
+                    using (filestream)
+                    {
+                        otherSounds.Add(SoundEffect.FromStream(filestream));
+                    }
+                    OtherSounds.Add(otherSounds[i].CreateInstance());
+                }
+            }
+        }
+        public List<SoundEffectInstance> NoCopySounds { get; }
+        public List<SoundEffectInstance> OtherSounds { get; }
+    }
+}
