bbc0d90775c4ab756f098d62a0a340aefe9473ae
Fokfritt <niitler19998@hotmail.com>
2020-04-28 20:30:01 +0200

    Började på QTE-klassen

---
+++ BlockBrawl/BlockBrawl/GameHandlerObjects/PlayObjects/QTE.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using BlockBrawl.Blocks;
+using BlockBrawl.Objects;
+
+
+namespace BlockBrawl
+{
+    class QTE
+    {
+        I i;
+        J j;
+        Z z;
+        S s;
+        L l;
+        O o;
+        T t;
+        Vector2 pos;
+        public QTE(Vector2 posP1, Vector2 posP2)
+        {
+            switch (SpawnRandomBlock())
+            {
+                case "J":
+                    j = new J(TextureManager.qteColor, Vector2.Zero);
+                    break;
+                case "I":
+                    i = new I(TextureManager.qteColor, Vector2.Zero);
+                    break;
+                case "T":
+                    t = new T(TextureManager.qteColor, Vector2.Zero);
+                    break;
+                case "O":
+                    o = new O(TextureManager.qteColor, Vector2.Zero);
+                    break;
+                case "L":
+                    l = new L(TextureManager.qteColor, Vector2.Zero);
+                    break;
+                case "S":
+                    s = new S(TextureManager.qteColor, Vector2.Zero);
+                    break;
+                case "Z":
+                    z = new Z(TextureManager.qteColor, Vector2.Zero);
+                    break;
+            }
+
+        }
+        public void Update(GameTime gameTime, InputManager im)
+        {
+            if()
+        }
+        public string SpawnRandomBlock()
+        {
+            Random rnd = new Random();
+            string[] feedRandomMachine = new string[] { "J", "I", "T", "O", "L", "S", "Z" };
+            return feedRandomMachine[rnd.Next(0, feedRandomMachine.Length)];
+        }
+        public void Draw(SpriteBatch spritebatch)
+        {
+            if(i != null)
+            {
+                i.Draw(spritebatch);
+            }
+        }
+
+    }
+}
